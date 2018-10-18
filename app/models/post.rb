5a55d29873b96d627c53936ec0b0b3f59293760b
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-10-18 02:28:02 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,17 +3,10 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validates :title, presence: true, :if => :clickbait?
-
-  # %w(/won't|believe|secret|top|guess/i)
+  validates :clickbait
 
 
-# ["%won't believe%", "%secret%", "%top guess%"]
-
-
-
-
-  def clickbait?
+  def clickbait
     regex = /won\'t|believe|secret|top|guess/i\w+\n/g/
     regex.match(title) =! nil
   end
