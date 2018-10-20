aabb455cb98dc8dd093e6b5a01b1a9755217ee44
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-10-20 06:36:06 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,16 +3,17 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
-  validates :title, presence: true, :in => ["won't believe", "secret", "top", "guess"].include?
+    validates :title, presence: true, inclusion: { :in => :clickbait }
+  # validates :title, presence: true, :in => ["won't believe", "secret", "top", "guess"].include?
 end
 
   # validates_inclusion_of :title, :in => ["won't believe", "secret", "top", "guess"].include?
-#   validates :title, presence: true, inclusion: { :in => :clickbait }
-#
-#
-#
-#   def clickbait
-#     regex = /\A(?:(believe)|(secret)|(top)|(guess)/i/)
-#     regex.match?(title)
-#   end
-# end
+
+
+
+
+  def clickbait
+    regex = [/(believe)|(secret)|(top)|(guess)/i/]
+    regex.match?(title)
+  end
+end
